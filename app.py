81cbeb25430c21e57ad5f6f949623f8273f7ac36
JJun0 <101166558+JJun0@users.noreply.github.com>
2023-05-19 17:04:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 import streamlit as st
+st.title('Math Proof Webpage')
+st.write('Welcome to the Math Proof Webpage! This page showcases mathematical proofs.')
 
- st.title('Math Proof Webpage')
-    st.write('Welcome to the Math Proof Webpage! This page showcases mathematical proofs.')
+st.header('Proof of Pythagoras Theorem')
+st.write('The Pythagorean theorem states that in a right-angled triangle, the square of the length of the hypotenuse is equal to the sum of the squares of the lengths of the other two sides.')
 
-    st.header('Proof of Pythagoras Theorem')
-    st.write('The Pythagorean theorem states that in a right-angled triangle, the square of the length of the hypotenuse is equal to the sum of the squares of the lengths of the other two sides.')
+st.subheader('Proof:')
+st.markdown('''
+    We start with a right-angled triangle with sides of lengths a, b, and c (where c is the hypotenuse).
+    According to the Pythagorean theorem, we have:
 
-    st.subheader('Proof:')
-    st.markdown('''
-        We start with a right-angled triangle with sides of lengths a, b, and c (where c is the hypotenuse).
-        According to the Pythagorean theorem, we have:
+    $$a^2 + b^2 = c^2$$
 
-        $$a^2 + b^2 = c^2$$
+    Now, let's prove this theorem using geometry:
 
-        Now, let's prove this theorem using geometry:
+    [Insert your proof steps here]
 
-        [Insert your proof steps here]
-
-        Thus, we have proved the Pythagorean theorem.
-    ''')
+    Thus, we have proved the Pythagorean theorem.
+''')
